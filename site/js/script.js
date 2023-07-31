f61fa7878bcb1662fd2987a08664affa5648e3b2
DanGrutsyuk <Dan_Gritsyuk>
2023-07-31 19:20:48 +0300

    add validation

--- site/js/script.js
+++ site/js/script.js
@@ -1,67 +1,16 @@
-window.onload = loadData;
-
-function loadData() {
-    let name = readCookie('name');
-    let age = readCookie('age');
-
-    if (name && age) {
-        let userData = document.getElementById('user-data');
-        let nameNode = document.createTextNode('Имя: ' + name);
-        let ageNode = document.createTextNode('Возраст: ' + age);
-        userData.appendChild(nameNode);
-        userData.appendChild(ageNode);
-
-        document.getElementById('after-submit').style.display = "block";
-        document.getElementById('input-data').style.display = "none";
-    }
-}
-
 function printName() {
-    let name = document.getElementById('nameInput').value;
-    let age = parseInt(document.getElementById('ageInput').value);
-    if (!name) {
-        alert('Укажите имя...');
-        return;
-    }
-    if (!age) {
-        alert('Укажите возраст...');
-        return;
-    }
-
-    alert('Привет, ' + name + '!\n');
-
-    switch (true) {
-        case age > 0 && age < 10:
-            alert('Тебе еще рано потреблять этот контент!');
-            break;
-        case age >= 10 && age < 18:
-            alert('Учи уроки, ' + name + '!');
-            break;
-        case age == 18:
-            alert('П-п, найс...');
-            break;
-        case age > 18 && age <= 30:
-            alert('Иди работай, ' + name + '.');
-            break;
-        case age > 30 && age < 101:
-            alert('Дед, прими таблетки!');
-            break;
-        default:
-            alert('Шутку понял, смешно.');
-    }
-
-    writeCookie('name', name);
-    writeCookie('age', age);
+    const form = document.querySelector('form');
+    form.addEventListener('submit', handleSubmit);
 }
 
 function changeColor() {
     var element = document.getElementById('colorful');
-    if (element.style.color == 'red') {
-        element.style.color = 'blue';
-        element.innerHTML = 'Это точно красный цвет!';
-    } else {
+    if (element.style.color == 'blue') {
         element.style.color = 'red';
         element.innerHTML = 'А этот синий цвет, не спорь!';
+    } else {
+        element.style.color = 'blue';
+        element.innerHTML = 'Это точно красный цвет!';
     }
 }
 
@@ -81,4 +30,71 @@ function readCookie(name) {
     var matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
     return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let requiredInputs = document.querySelectorAll('[required]');
+    let isFormValid = true;
+    requiredInputs.forEach(input => {
+        if (!input.value) {
+            input.classList.add('error');
+            isFormValid = false;
+        } else {
+            input.classList.remove('error');
+        }
+    });
+    if (isFormValid) {
+        alertData(requiredInputs);
+    }
+}
+
+function alertData(requiredInputs) {
+    let name = requiredInputs[0].value;
+    let age = parseInt(requiredInputs[1].value);
+    let validUser = false;
+
+    alert('Привет, ' + name + '!\n');
+
+    switch (true) {
+        case age > 0 && age < 10:
+            alert('Тебе еще рано потреблять этот контент!');
+            break;
+        case age >= 10 && age < 18:
+            alert('Учи уроки, ' + name + '!');
+            break;
+        case age == 18:
+            alert('П-п, найс...');
+            validUser = true;
+            break;
+        case age > 18 && age < 30:
+            alert('Иди работай, ' + name + '.');
+            break;
+        case age >= 30 && age < 101:
+            alert('Дед, прими таблетки!');
+            break;
+        default:
+            alert('Шутку понял, смешно.');
+    }
+
+    if (validUser) {
+        let ul = document.getElementById('user-data');
+        let ocum = "";
+
+        ocum += `<li>
+                    Имя: ${name}
+                </li>
+                <li>
+                    Возраст: ${age}
+                </li>`;
+        ul.innerHTML = ocum;
+
+
+
+        document.getElementById('after-submit').style.display = "block";
+        document.getElementById('input-data').style.display = "none";
+
+        document.getElementById('after-submit').style.display = "block";
+        document.getElementById('input-data').style.display = "none";
+    }
 }
